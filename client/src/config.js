19f4a287b98b1c942d50164f3e833155667b0862
REHAMAN2094 <rehaman2094@gmail.com>
2025-08-21 09:37:34 +0530

    cm

--- client/src/config.js
+++ client/src/config.js
@@ -7,11 +7,13 @@ export const config = {
     
     // Production - replace with your actual API server URL
     // TODO: Change this to your deployed API server URL
-    production: 'http://localhost:8080', // Temporary: change this after deploying your API server
+    production: 'https://smart-helpdesk-backend.onrender.com', // Temporary: change this after deploying your API server
     
     // Staging - if you have a staging environment
     staging: 'https://staging-api.your-domain.com'
   },
+  
+
   
   // Get the appropriate API URL based on environment
   getApiUrl: () => {
